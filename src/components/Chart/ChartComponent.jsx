a25784e6487a2ae49e7822a1014555bd8126be37
Gabriel Antonio <oliveiragabr@outlook.com>
2021-07-20 02:51:38 -0400

    chart to show global stats added

--- src/components/Chart/ChartComponent.jsx
+++ src/components/Chart/ChartComponent.jsx
@@ -1,12 +1,80 @@
- 
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
-const ChartComponent = () => {
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+const ChartComponent = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchDailyDataset = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+
+    fetchDailyDataset();
+  }, []);
+
+  /** BAR CHART COMPONENT FOR COUNTRY DATA */
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
+  /** LINE CHART COMPONENT FOR GLOBAL DATA */
+  const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },  {
+            data: dailyData.map((data) => data.recovered),
+            label: 'Recovered',
+            borderColor: 'green',
+            backgroundColor: 'rgba(0, 255, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
+
   return (
-    <div>
-      <p>Chart works</p>
+    <div className={styles.container}>
+      {country ? barChart : lineChart}
     </div>
-  )
-}
+  );
+};
 
 export default ChartComponent
